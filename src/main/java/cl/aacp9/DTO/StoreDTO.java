968fb8ddc918838161a36edf879f8edb19b3073f
aacp9 <aacp9439@gmail.com>
2025-08-25 09:27:37 -0400

    subiendo cambios de lombok

--- src/main/java/cl/aacp9/DTO/StoreDTO.java
+++ src/main/java/cl/aacp9/DTO/StoreDTO.java
@@ -13,35 +13,4 @@ public class StoreDTO {
 	private Long storeId;
 	private String storeName;
 
-	//CONSTRUCCTOR	
-	
-	public StoreDTO(Long storeId, String storeName) {
-		super();
-		this.storeId = storeId;
-		this.storeName = storeName;
-	}
-//	
-	public StoreDTO() {
-		super();
-	}
-
-	//GETTER AND SETTER	
-	public Long getStoreId() {
-		return storeId;
-	}
-	public void setStoreId(Long storeId) {
-		this.storeId = storeId;
-	}
-	public String getStoreName() {
-		return storeName;
-	}
-	public void setStoreName(String storeName) {
-		this.storeName = storeName;
-	}
-	
-	
-	
-
-	
-	
 }
